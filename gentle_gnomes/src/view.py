2516feb61752a9575705eced693afbfd3d351f30
MarkKoz <KozlovMark@gmail.com>
2019-07-04 23:11:09 -0700

    Add import reorder pre-commit hook

--- gentle_gnomes/src/view.py
+++ gentle_gnomes/src/view.py
@@ -1,10 +1,12 @@
 import json
 
-from flask import abort, current_app as app, Blueprint, render_template, request, flash
+import flask
+from flask import current_app as app
+from flask import render_template
 
 from . import indicator
 
-bp = Blueprint('view', __name__, url_prefix='/')
+bp = flask.Blueprint('view', __name__, url_prefix='/')
 
 
 @bp.route('/')
@@ -15,11 +17,11 @@ def index():
 @bp.route('/search', methods=['POST'])
 def search():
     try:
-        location = json.loads(request.form['location'])
+        location = json.loads(flask.request.form['location'])
         latitude = location['lat']
         longitude = location['lng']
     except (json.JSONDecodeError, KeyError):
-        return abort(400)
+        return flask.abort(400)
 
     city = app.azavea.get_nearest_city(latitude, longitude)
     if city:
@@ -28,7 +30,7 @@ def search():
 
         results = '\n'.join(f'{i.label}: {i.rate}' for i in top)
     else:
-        flash('Location not found.')
+        flask.flash('Location not found.')
         results = None
 
     return render_template('view/index.html', city=str(city), results=results)
